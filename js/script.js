1ed054b909497b32c0ab6865b3286fdf26017b56
Gustavo Pimentel <gustavoafpimentel@gmail.com>
2024-01-07 21:26:23 -0300

    UI do score e da tela de game over adicionados.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
+const score = document.querySelector(".score--value");
+const finalScore = document.querySelector(".final-score > span");
+const menu = document.querySelector(".menu-screen");
+const buttonPlay = document.querySelector(".btn-play");
+
 const audio = new Audio('../assets/audio.mp3');
 
 const size = 30;
@@ -132,8 +137,12 @@ const checkCollision = () => {
     });
 
     if(wallCollision || selfCollision) {
-        alert('Você perdeu!');
+        gameOver();
     }
+}
+
+const gameOver = () => {
+    direction = undefined;
 }
 
 const gameLoop = () => {
